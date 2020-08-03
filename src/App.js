9cf026e702d7f752cf1ce88bd27eda6d384a9f44
Nadeesh Peiris <nash.ucsc@gmail.com>
2020-08-03 19:39:02 +0530

    header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
-
+import { districts, divisions } from './util';
+console.log(districts);
 const App = () => {
   const [results, setResults] = useState([]);
+  const [selectedDistrict, setDistrict] = useState(0);
+  const [selectedDivision, setDivision] = useState(0);
 
   useEffect(() => {
     const id = setInterval(() => {
+
       fetch("/results")
       .then(res => res.json())
       .then(
@@ -16,13 +20,46 @@ const App = () => {
         }
       )
       setResults([]);
-    }, 3000);
+    }, 60000);
     return () => clearInterval(id);
   }, []);
 
+  function setDistrictVal(e) {
+    setDistrict(e.currentTarget.value);
+    setDivision(0);
+  }
+
+  function setDivisionVal(e) {
+    setDivision(e.currentTarget.value);
+  }
+
   return (
-    <div>
-      {results}
+    <div className="container">
+      <div className="header">
+        <h2 className="title">General Election 2020</h2>
+        <div className="selection">
+          <select name="district" id="district" className="district" onChange={setDistrictVal}>
+            {districts && districts.map((district) => (
+              <option key={district.id} value={district.id}>
+                {district.name}
+              </option>
+            ))}
+          </select>
+
+          {selectedDistrict>0 ? (<select name="division" id="division" className="division" onChange={setDivisionVal}>
+            <option defaultValue value="0"> district </option>
+            {selectedDistrict && divisions[selectedDistrict].map((division) => (
+              <option key={division.id} value={division.id}>
+                {division.name}
+              </option>
+            ))}
+          </select>) : ''}
+        </div>
+      </div>
+      <Result
+        selectedDistrict={selectedDistrict}
+        selectedDivision={selectedDivision}
+      />
     </div>
   );
 };
