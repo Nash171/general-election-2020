00a8c29ed79f99f90a152bdea8ffe4ab5e024ab4
Nadeesh Peiris <nash.ucsc@gmail.com>
2020-08-03 17:38:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      fetch("/results")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setResults(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+      setResults([]);
+    }, 3000);
+    return () => clearInterval(id);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {results}
     </div>
   );
-}
+};
 
 export default App;
